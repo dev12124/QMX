81e6ea29bc337aa7d406c79350c9e40270f71b2b
dev12124 <joaoanandalima@gmail.com>
2026-09-05 15:58:02 -0300

    Sign and Update build.bat

--- Compile and Link/build.bat
+++ Compile and Link/build.bat
@@ -1,7 +1,7 @@
 @echo off
 llvm-rc ..\resource.rc /fo resource.res
 clang++ ..\main.cpp resource.res -o ..\qmx_engine.exe -O3 -std=c++17 -fuse-ld=lld -Xlinker /subsystem:windows -Xlinker -entry:WinMainCRTStartup -l d3d12 -l dxgi -l user32
-signtool sign /n "QMX Corporation" /fd SHA256 /t http://digicert.com qmx_engine.exe
+signtool sign /n "QMX Corporation" /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 qmx_engine.exe
 
 if %errorlevel% equ 0 (
     echo [QMX ENGINE] qmx_engine.exe generated successfully!
