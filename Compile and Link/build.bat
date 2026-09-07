8e28bc918682171d54bdc2c6fb079a5c330b06aa
dev12124 <joaoanandalima@gmail.com>
2026-09-06 23:19:42 -0300

    Update in build.bat and Executable Engine

--- Compile and Link/build.bat
+++ Compile and Link/build.bat
@@ -1,10 +1,12 @@
 @echo off
 llvm-rc ..\resource.rc /fo resource.res
-clang++ ..\main.cpp resource.res -o ..\qmx_engine.exe -O3 -std=c++17 -fuse-ld=lld -Xlinker /subsystem:windows -Xlinker -entry:WinMainCRTStartup -l d3d12 -l dxgi -l user32
-signtool sign /n "QMX Corporation" /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 qmx_engine.exe
+clang++ ..\main.cpp resource.res -o ..\qmx_engine.exe -O3 -std=c++17 -fno-rtti -fno-exceptions -flto -fuse-ld=lld -Xlinker /subsystem:windows -Xlinker -entry:WinMainCRTStartup -l d3d12 -l dxgi -l user32
+
+rem Ajustado para a URL completa do servidor de timestamp da DigiCert
+signtool sign /n "QMX Corporation" /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 ..\qmx_engine.exe
 
 if %errorlevel% equ 0 (
-    echo [QMX ENGINE] qmx_engine.exe generated successfully!
+    echo [QMX ENGINE] qmx_engine.exe generated successfully with DigiCert Timestamp!
 ) else (
     echo [QMX ENGINE] Check the Errors
 )
